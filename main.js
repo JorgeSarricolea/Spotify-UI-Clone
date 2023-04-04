09bef276ff198283cb3e0f91345f784d51168743
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-04-03 18:17:02 -0600

    Play button js

--- main.js
+++ main.js
@@ -14,3 +14,44 @@ let topbarHeight = topBar.offsetHeight;
 // Add a height based paddingTop to the main content
 const mainContent = document.querySelector('.main-content');
 mainContent.style.paddingTop = `${topbarHeight + 20}px`;
+
+/* PLAY BUTTON */
+
+const containerConcentracion = document.querySelectorAll(
+	'.card-concentracion'
+);
+const containerSpotifyPlaylists = document.querySelectorAll(
+	'.card-spotify-playlists'
+);
+
+// Function to repeat
+const createButton = card => {
+	// Button creation
+	const button = document.createElement('button');
+	button.innerHTML = '<i class="fa-solid fa-play"></i>';
+
+	// Add button
+	card.appendChild(button);
+
+	// Hide button
+	button.style.display = 'none';
+	button.classList.add('btn-play');
+
+	// Hover effect
+	card.addEventListener('mouseover', () => {
+		button.style.display = 'block';
+	});
+
+	card.addEventListener('mouseout', () => {
+		button.style.display = 'none';
+	});
+};
+
+// ForEach per rows of cards
+containerConcentracion.forEach(card => {
+	createButton(card);
+});
+
+containerSpotifyPlaylists.forEach(card => {
+	createButton(card);
+});
